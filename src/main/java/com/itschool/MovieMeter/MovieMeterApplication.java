ec63acd89b23ebf3657786ed0894df560c63e7b6
AlexandruC97 <63367791+AlexandruC97@users.noreply.github.com>
2024-07-21 16:36:49 +0300

    Original commit

---
+++ src/main/java/com/itschool/MovieMeter/MovieMeterApplication.java
@@ -0,0 +1,13 @@
+package com.itschool.MovieMeter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieMeterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieMeterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itschool/MovieMeter/MovieMeterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itschool.MovieMeter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieMeterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
